b519548a5bf53a2024177c5a8439b89ecb2aee48
Mauricio Tourn <tourn1@Mac-mini-de-Mauricio.local>
2026-05-08 10:10:53 -0300

    reorganizacion del proyecto

--- android/app/src/main/java/com/mauritv/movies/MainActivity.java
+++ android/app/src/main/java/com/mauritv/movies/MainActivity.java
@@ -1,5 +1,25 @@
 package com.mauritv.movies;
 
+import android.os.Bundle;
+import android.view.View;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Asegurar que el WebView sea el foco para que los direccionales funcionen con tabindex
+        View webView = getBridge().getWebView();
+        if (webView != null) {
+            webView.setFocusable(true);
+            webView.setFocusableInTouchMode(true);
+            webView.requestFocus();
+        }
+    }
+}
+
